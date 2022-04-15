6cc092c3355047ff6773b08eb2137121bcbde8fe
Cuong Nguyen <cuong.nguyen@stunited.vn>
2022-04-15 16:20:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -51,10 +51,10 @@ export default function App() {
     content: {
       top: '50%',
       left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
+      height: 440,
       transform: 'translate(-50%, -50%)',
+      padding: "0px 10px",
+      margin: 0,
     },
   };
 
@@ -67,17 +67,44 @@ export default function App() {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2>{itemClick?.item?.type} ID: {itemClick?.item?.id}</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
-      </Modal>
+        <div style={{ width: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center', height: 50, padding: "10px 0px", borderBottom: "1px solid #EDEDED", }}>
+          <p className='title-modal'>{itemClick?.item?.type} ID: {itemClick?.item?.id}</p>
+        </div>
+        <div style={{ width: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center', height: 300, borderBottom: "1px solid #EDEDED", flexWrap: "wrap" }}>
+          <div className='div-input'>
+            <label className='lable-input'>Type</label>
+            <input value={itemClick?.item?.itemType} className='input-value'></input>
+          </div>
+          <div className='div-input'>
+            <label className='lable-input'>Height</label>
+            <input value={itemClick?.item?.size?.height} className='input-value'></input>
+          </div>
+          <div className='div-input'>
+            <label className='lable-input'>Width</label>
+            <input value={itemClick?.item?.size?.width} className='input-value'></input>
+          </div>
+          <div className='div-input'>
+            <label className='lable-input'>position-x</label>
+            <input value={itemClick?.item?.position?.x} className='input-value'></input>
+          </div>
+          <div className='div-input'>
+            <label className='lable-input'>position-y</label>
+            <input value={itemClick?.item?.position?.y} className='input-value'></input>
+          </div>
+        </div>
+        <div style={{ width: "100%", display: 'flex', justifyContent: 'flex-end', alignItems: 'center', height: 50, paddingTop: "10px" }}>
+          <div onClick={() => {
+            closeModal();
+          }} style={{ width: 100, height: 40, backgroundColor: '#337ab7', justifyContent: 'center', alignItems: "center", display: 'flex', cursor: "pointer", marginRight: 20, border: "1px solid #EDEDED", borderRadius: 4 }}>
+            <p style={{ color: "#fff", fontWeight: "600" }}>Save</p>
+          </div>
+          <div onClick={() => {
+            closeModal();
+          }} style={{ width: 100, height: 40, backgroundColor: '#f9f9f9', justifyContent: 'center', alignItems: "center", display: 'flex', cursor: "pointer", border: "1px solid #EDEDED", borderRadius: 4 }}>
+            <p style={{ color: "#000", fontWeight: "600" }}>Cancel</p>
+          </div>
+        </div>
+      </Modal >
       <Diagram
         // is on change
         // onRequestEditOperation={(a, b, c) => {
@@ -144,7 +171,7 @@ export default function App() {
       }} style={{ width: 100, height: 46, backgroundColor: '#337ab7', position: "absolute", top: 9, right: 9, justifyContent: 'center', alignItems: "center", display: 'flex', cursor: "pointer", zIndex: 1000 }}>
         <p style={{ color: "#fff", fontWeight: "600" }}>Save</p>
       </div>
-    </div>
+    </div >
 
   )
 }
